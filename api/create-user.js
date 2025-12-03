9f95352ba9c75c8636260493311bc7cb31f9cdef
Phạm Thanh Nhân <210466144+techmoodbiz@users.noreply.github.com>
2025-12-03 10:41:01 +0700

    Update create-user.js

--- api/create-user.js
+++ api/create-user.js
@@ -101,7 +101,7 @@ module.exports = async function handler(req, res) {
             name,
             email,
             role,
-            ownedBrandId: role === "brand_owner" ? ownedBrandId : null,
+            ownedBrandIds: role === "brand_owner" ? ownedBrandIds || [] : [],
             assignedBrandIds: role === "content_creator" ? assignedBrandIds || [] : [],
             createdAt: admin.firestore.FieldValue.serverTimestamp(),
             createdBy: currentUser.uid,
